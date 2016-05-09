f73cc0508eadecf6d12f2260b06a3c66bda3d04c
aanciaes <aanciaes.miguel@gmail.com>
2016-05-09 21:03:56 +0100

    Main Class Created

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Reads all the inputs and produces outputs
+ * @author miguel anciaes
+ * m.anciaes@campus.fct.unl.pt
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+}
